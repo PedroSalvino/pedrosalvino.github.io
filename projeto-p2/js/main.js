c23dd9212908a47374994319937ae35ad2d67dd5
PedroSalvino <pedro_luiz7199@outlook.com>
2020-11-21 14:56:30 -0300

    Adicionando as fotos

--- projeto-p2/js/main.js
+++ projeto-p2/js/main.js
@@ -1,18 +1,18 @@
 function principal(){
-
-	// function modal1(){
-	// 	document.querySelector('#filme1').addEventListener('click', dadosModal("https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-143692/"));	
-	// }
-
-	// function modal2(){
-	// 	document.querySelector('#filme2').addEventListener('click', dadosModal("https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-114782/"));	
-	// }
-	
-	// // modal1();
-	// modal2();
+	//*FILME 1
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-143692/','https://cors-anywhere.herokuapp.com/http://www.adorocinema.com/filmes/filme-143692/fotos/detalhe/?cmediafile=20028688','titulo1','foto1');
+	//*FILME 2
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-114782/','https://cors-anywhere.herokuapp.com/http://www.adorocinema.com/filmes/filme-114782/fotos/detalhe/?cmediafile=21154448','titulo2','foto2');
+	//*FILME 3
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-130440/','https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-130440/fotos/detalhe/?cmediafile=21085451','titulo3','foto3');
+	//*FILME 4
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-232669/','https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-232669/fotos/detalhe/?cmediafile=21614858','titulo4','foto4');
+	//*FILME 5
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-41245/','https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-41245/fotos/detalhe/?cmediafile=20230712','titulo5','foto5');
+	//*FILME 6
+	dadosFilmes('https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-196666/','https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-196666/fotos/detalhe/?cmediafile=21047331','titulo6','foto6');
 }
-
-
+//*
 function dadosModal(url){
 	fetch(url,{
 		mode: 'cors',
@@ -52,12 +52,9 @@ function dadosModal(url){
 		}
 	);
 }
-
-window.onload = principal;
-
-
-	//*FILME 1
-	/*fetch("https://cors-anywhere.herokuapp.com/https://www.adorocinema.com/filmes/filme-143692/",{
+//*
+function dadosFilmes(urltexto, urlfoto,idtitulo,idfoto){
+	fetch(urltexto,{
 		mode: 'cors',
 		headers: {  
             "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -69,14 +66,34 @@ window.onload = principal;
 			let dom = new DOMParser();
 			let doc = dom.parseFromString(x,"text/html");
 			//*
-			let titulo = document.querySelector("#titulo1");
-			// let titulomodal=document.querySelector("#filmeModalLabel");
-			//var foto = document.querySelector("#foto1");
+			let titulo = document.querySelector("#"+idtitulo);
 			titulo.innerHTML=doc.querySelector(".titlebar-title").innerHTML;
-			// titulomodal.innerHTML=doc.querySelector(".titlebar-title").innerHTML;
-			//foto.src=doc.querySelector(".shot-img").src;
 		}
 	)
+	//*
+	fetch(urlfoto, {
+		mode: 'cors',
+		headers: {  
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+				"Access-Control-Allow-Origin":"*"
+        }
+	})
+	.then(resp => resp.text())
+	.then(x => {
+			let dom = new DOMParser();
+			let doc = dom.parseFromString(x,"text/html");
+			//*
+			let foto = document.querySelector("#"+idfoto);
+			foto.src=doc.querySelector(".gallery-media").src;
+		}
+	)
+}
+//*
+window.onload = principal;
+
+
+	//*FILME 1
+	/*
 	//*FOTO - FILME 1
 	fetch("https://cors-anywhere.herokuapp.com/http://www.adorocinema.com/filmes/filme-143692/fotos/detalhe/?cmediafile=20028688", {
 		mode: 'cors',
